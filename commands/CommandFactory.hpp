e9c703525e85f12b01cc7a2af04ab09b0d870b40
Ravi chandra <githubravi01@gmail.com>
2025-07-28 15:01:17 +0530

    Updated_Command_Factory

--- commands/CommandFactory.hpp
+++ commands/CommandFactory.hpp
@@ -13,6 +13,18 @@
 #include "GetCommand.hpp"
 #include "DelCommand.hpp"
 
+// Additional Commands for Redis
+#include "LPushCommand.hpp"
+#include "LPopCommand.hpp"
+#include "RPushPopCommand.hpp"
+#include "LlenCommand.hpp"
+#include "setCachingCommands.hpp"
+#include "HGetCommand.hpp"
+#include "HSetCommand.hpp"
+#include "StringIntegerCommand.hpp"
+
+#include "CachingAdapter.hpp"
+
 // Socket Commands
 #include "SubscribeCommand.hpp"
 #include "UnsubscribeCommand.hpp"
@@ -35,6 +47,59 @@ public:
         registry["SUBSCRIBE"] = &SubscribeCommand::create;
         registry["UNSUBSCRIBE"] = &UnsubscribeCommand::create;
         registry["PUBLISH"] = &PublishCommand::create;
+
+        // Additional Commands Creation
+        registry["LPUSH"] = []() {
+            return make_unique<CachingAdapter>(lPushCommand::create());
+        };
+
+        registry["LPOP"] = []() {
+            return make_unique<CachingAdapter> (lPopCommand::create());
+        };
+
+        registry["RPUSH"] = []() {
+            return make_unique<CachingAdapter> (rPush::create());
+        };
+
+        registry["RPOP"] = []() {
+            return make_unique<CachingAdapter> (rPop::create());
+        };
+
+        registry["HSET"] = []() {
+            return make_unique<CachingAdapter> (hSetCommand::create());
+        };
+
+        registry["HGET"] = []() {
+            return make_unique<CachingAdapter> (hGetCommand::create());
+        };
+
+        registry["SADD"] = []() {
+            return make_unique<CachingAdapter> (setAddCommand::create());
+        };
+
+        registry["SREM"] = []() {
+            return make_unique<CachingAdapter> (setRemCommand::create());
+        };
+
+        registry["INCR"] = []() {
+            return make_unique<CachingAdapter> (IncrCommand::create());
+        };
+
+        registry["DECR"] = []() {
+            return make_unique<CachingAdapter> (DecrCommand::create());
+        };
+
+        registry["SMEMBER"] = []() {
+            return make_unique<CachingAdapter> (setMemberCommand::create());
+        };
+
+        registry["LLEN"] = []() {
+            return make_unique<CachingAdapter> (lLenCommand::create());
+        };
+
+        registry["LRANGE"] = []() {
+            return make_unique<CachingAdapter> (lRangeCommand::create());
+        };
     }
 
     unique_ptr<ICommand> create(const string &name) const
